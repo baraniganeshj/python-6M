e78b5623d1cfa6bf29f9536ba1113183725be1e0
J BARANI GANESH <jb.ganesh@op.iitg.ac.in>
2026-06-30 13:32:34 +0000

    Day 1: Completed first Python network info practice

---
+++ Week-01/day01_practice01.py
@@ -0,0 +1,10 @@
+device_name = "Router-01"
+ip_address = "10.10.10.1"
+status = "UP"
+location = "Chennai DC"
+
+print("Device Name:", device_name)
+print("IP Address:", ip_address)
+print("Status:", status)
+print("Location:", location)
+
