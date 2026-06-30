c70608ffed84c5fa8ac867bd7c98ca1cd000bbeb
J BARANI GANESH <jb.ganesh@op.iitg.ac.in>
2026-06-30 13:50:14 +0000

    Day 2: Practiced variables with shift information

---
+++ Week-01/day02_shift_info.py
@@ -0,0 +1,16 @@
+employee_name = "Barani"
+shift = "Evening"
+team = "Network Operations"
+role = "Senior Analyst"
+current_task = "Following up with ISP vendor"
+
+shift = "Night"
+role = "AIOps Learner"
+current_task = "Practicing Python variables"
+
+print("Employee Name:", employee_name)
+print("Shift:", shift)
+print("Team:", team)
+print("Role:", role)
+print("Current Task:", current_task)
+
