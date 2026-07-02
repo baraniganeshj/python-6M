6e49a893a38c64ba6b62df121edda77087bd2ab7
J BARANI GANESH <jb.ganesh@op.iitg.ac.in>
2026-07-02 20:15:40 +0000

    Day04: Practiced user input and average calculation

---
+++ Week-01/day04_user_input.py
@@ -0,0 +1,17 @@
+name = input("Enter your Name: ")
+hours_worked = int(input("Enter Hours Worked: "))
+assigned_tickets = int(input("Enter Assigned Tickets: "))
+tickets_closed = int(input("Enter Tickets Closed: "))
+
+
+average_tickets = tickets_closed / hours_worked
+remaining_tickets = assigned_tickets - tickets_closed
+
+print(f"Shift Summary for {name}")
+print(f"Hours Worked: {hours_worked}")
+print(f"Tickets Closed: {tickets_closed}")
+print(f"Remaining Tickets: {remaining_tickets}")
+print(f"Average Tickets Per Hour: {average_tickets}")
+
+
+
