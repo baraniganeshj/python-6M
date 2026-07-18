d4ae1c4c99fc7f455fa8fc570668c073998eaf13
J BARANI GANESH <jb.ganesh@op.iitg.ac.in>
2026-07-18 21:44:54 +0000

    Program 08: Practiced lists and data storage

---
+++ program08_lists.py
@@ -0,0 +1,20 @@
+devices = ["Router-1", "Switch-1", "Firewall-1", "Server-1"]
+
+print("First Device:", devices[0])
+print("Second Device:", devices[1])
+
+print("Total Devices:", len(devices))
+
+devices.append("Router-2")
+
+print("\nDevices Added:", devices[-1])
+
+print("Total Devices After addition:", len(devices))
+
+print("\nLast Device:", devices[-1])
+
+ISP = ["AT&T", "Verizon", "Lumen", "Comcast"]
+
+print("\nFirst ISP:", ISP[0])
+print("Last ISP:", ISP[-1])
+print("Total ISP Count:", len(ISP))
