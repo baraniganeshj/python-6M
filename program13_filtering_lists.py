5432e8445c7aa7072d8319271c804487441ed363
J BARANI GANESH <jb.ganesh@op.iitg.ac.in>
2026-07-19 21:34:05 +0000

    Program 13: Practiced filtering lists and list comprehensions

--- program13_filtering_lists.py
+++ program13_filtering_lists.py
@@ -1,19 +1,14 @@
 All_Alerts = ['INFO', 'WARNING', 'CRITICAL', 'INFO', 'CRITICAL', 'WARNING']
-
 print(f"All Alerts:\n {All_Alerts}")
 print()
-
 cirtical_alerts = [alert for alert in All_Alerts if alert == 'CRITICAL']
-
 print(f"Critical Alerts:\n {cirtical_alerts}")
 print()
 critical_count = cirtical_alerts.count('CRITICAL')
 print(f"Total Critical Alerts: {(critical_count)}")
 
 devices = ["Router-1", "Switch-1", "Firewall-1", "Server-1"]
-
 Routers = [device for device in devices if device == 'Router-1']
-
 print()
 print(f"Routers:\n {Routers}")
 
